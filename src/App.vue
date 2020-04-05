00f421eb83c0c94f57a18aa3f5cc08f54fd06d80
Alex <slicer256@gmail.com>
2020-04-05 16:40:21 +0300

    more basic setup

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,46 @@
 <template>
     <div id="app">
-        <img src="./assets/logo.png">
+        <h1>Библия</h1>
         <h1>{{ msg }}</h1>
-
     </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     name: 'app',
     data () {
         return {
-            msg: 'Welcome to Your Vue.js App'
+            msg: 'Welcome to Your Vue.js App',
+            chapters: [],
         }
-    }
+    },
+
+    created() {
+
+    },
+
+    methods: {
+        getAllPosts() {
+            axios.get(this.endpoint).then(response => {
+                this.posts = response.data;
+            }).catch(error => {
+                console.error(error);
+            })
+        }
+    },
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 
 a {
