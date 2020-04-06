76dc360126bdb27786648a0645cf44858e037bfa
Alex <slicer256@gmail.com>
2020-04-06 22:57:47 +0300

    user auth API

--- api/index.php
+++ api/index.php
@@ -31,6 +31,37 @@ if ($_GET['action'] === 'chapter') {
     }
 }
 
+if ($_GET['action'] === 'auth') {
+    $request_body = file_get_contents('php://input');
+    $request_body = json_decode($request_body, true);
+
+    $email = $db->filter($request_body['email'], 'email');
+    $password = md5($request_body['password']);
+    $row = $db->get_row("SELECT * FROM users WHERE email='{$email}' AND password='{$password}' LIMIT 1");
+
+    if ($row) {
+        // generate new session key for user
+        $session = md5($email . $password . time());
+        $db->update('users', $row['id'], ['session' => $session]);
+
+        $data = [
+            'session' => $session,
+            'user_name' => $row['name'],
+            'user_email' => $row['email'],
+        ];
+    }
+    elseif ($db->get_row("SELECT id FROM users WHERE email='{$email}'") != false) {
+        $data = [
+            'error' => "Неверный пароль.",
+        ];
+    }
+    else {
+        $data = [
+            'error' => "Пользователь с email {$email} не найден.",
+        ];
+    }
+}
+
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 echo json_encode($data);
