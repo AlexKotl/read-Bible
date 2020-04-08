fe8f7e9daffdcc680264961df9a51852b2ce8366
Alex <slicer256@gmail.com>
2020-04-08 12:08:45 +0300

    static pie chart

--- src/components/StatisticsShort.vue
+++ src/components/StatisticsShort.vue
@@ -1,12 +1,27 @@
 <template>
     <div>
-        Stats
+        <PieChart :chart-data="datacollection"></PieChart>
     </div>
 </template>
 
 <script>
-
+import PieChart from './charts/PieChart.vue';
 export default {
+    components: { PieChart },
+    data () {
+        return {
+            datacollection: null
+        }
+    },
+    mounted() {
+        this.datacollection = {
+            labels: ['Прочитано', 'Не прочитано'],
+            datasets: [{
+                backgroundColor: ['#5da85d', '#ebe293'],
+                data: [3, 5]
+            }]
+        }
+    }
 }
 </script>
 
---
+++ src/components/charts/PieChart.vue
@@ -0,0 +1,12 @@
+<script>
+import { Pie, mixins } from 'vue-chartjs';
+
+export default {
+    extends: Pie,
+    mixins: [mixins.reactiveProp],
+    props: ['options'],
+    mounted () {
+        this.renderChart(this.chartdata, this.options);
+    }
+}
+</script>
