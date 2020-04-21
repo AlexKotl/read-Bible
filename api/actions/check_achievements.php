cb23d59bbb1d3c2e151a2dc619545ecca8497866
Alex <slicer256@gmail.com>
2020-04-21 12:46:00 +0300

    added API scripts to detect 4 achievements

--- api/actions/check_achievements.php
+++ api/actions/check_achievements.php
@@ -1,35 +1,81 @@
 <?php
 
+// record achievement
+function achieve($data) {
+    return [
+        'id' => $data['id'],
+        'name' => $data['name'],
+        'title' => $data['title_ru'],
+    ];
+}
+
 if (!$row_user) {
     die("User auth failed");
 }
 
-$res = $db->query("SELECT * FROM achievements");
-while ($row = $db->fetch($res)) {
+$data = [];
+
+$res_ach = $db->query("SELECT * FROM achievements");
+while ($row_ach = $db->fetch($res_ach)) {
     // Read one chapter
-    if ($row['name'] === 'first_chapter') {
+    if ($row_ach['name'] === 'first_chapter') {
         $count = $db->get_row("SELECT COUNT(*) FROM users_chapters WHERE user_id='{$row_user['id']}' AND is_read='1'");
         if ($count > 0) {
-            $data[] = [
-                'title' => $row['title_ru'],
-            ];
+            $data[] = achieve($row_ach);
         }
     }
 
     // Read one book
-    if ($row['name'] === 'first_book') {
+    if ($row_ach['name'] === 'first_book') {
+        $whole_chapters_count = $db->get_row("SELECT COUNT(*) FROM (
+            SELECT books.*, count(DISTINCT chapters.id) AS chapters_count, count(DISTINCT users_chapters.id) AS user_count  FROM books
+            LEFT JOIN chapters ON chapters.book_id=books.id
+            LEFT JOIN users_chapters ON chapters.id=users_chapters.chapter_id AND users_chapters.user_id='{$row_user['id']}' AND users_chapters.is_read=1
+            GROUP BY books.id
+        ) subtable WHERE user_count = chapters_count");
 
+        if ($whole_chapters_count > 0) {
+            $data[] = achieve($row_ach);
+        }
+    }
+
+    // get list of longest days streak when user readed
+    $longest = $current = 1;
+    $res = $db->query("SELECT *, DATE(date_created) as date FROM users_chapters
+        WHERE users_chapters.user_id='{$row_user['id']}' AND users_chapters.is_read=1
+        ORDER BY date");
+    while ($row = $db->fetch($res)) {
+        if (!$prev) {
+            $prev = $row;
+            continue;
+        }
+
+        $days_passed = date_diff(date_create($row['date']), date_create($prev['date']))->days;
+        if ($days_passed === 1) {
+            $current++;
+        }
+        elseif ($days_passed > 1) {
+            if ($longest < $current) {
+                $longest = $current;
+            }
+            $current = 1;
+        }
+
+        $prev = $row;
     }
 
     // 7 days of reading
-    if ($row['name'] === '7_days') {
-
+    if ($row_ach['name'] === '7_days') {
+        if ($longest >= 7) {
+            $data[] = achieve($row_ach);
+        }
     }
 
     // 30 days of reading
-    if ($row['name'] === '30_days') {
-
+    if ($row_ach['name'] === '30_days') {
+        if ($longest >= 30) {
+            $data[] = achieve($row_ach);
+        }
     }
 }
 
-$data = [];
